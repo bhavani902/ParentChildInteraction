40ddaf788353e71b95d360b8d23c3aebb7d11b93
Bhavani <govrayapallybhavani@gmail.com>
2021-05-09 12:23:37 +0530

    product comp is finished

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -14,38 +14,48 @@ export class ProductsComponent implements OnInit {
   }
   products:Product[]=[
     {
-      productTitle:"Technology-1",
-      description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-      productImg:"https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg"
+      productTitle:"Maruti Ertiga",
+      description:"1462CC engine,60Liters of fuel capacity,Mileage:17.99 Kmpl,Dimensions(L*W*H):4395*1735*1690 mm^3",
+      productImg:"https://stimg.cardekho.com/images/carexteriorimages/630x420/Maruti/Ertiga/7048/1611730397556/front-left-side-47.jpg"
       },
       {
-      productTitle:"Technology-2",
-      description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-      productImg:"https://apicms.thestar.com.my/uploads/images/2020/08/24/831125.jpg"
-      },
-      
-      {
-      productTitle:"Technology-3",
-      description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-      productImg:"https://itconsultingindubai.files.wordpress.com/2016/03/choosing-a-technology-solution.jpg"
+      productTitle:"Hyundai Verna",
+      description:"1493CC engine,fuel type is diesel,Mileage:17.99 Kmpl,Dimensions(L*W*H):4,440*1,729*1,475 mm",
+      productImg:"https://stimg.cardekho.com/images/carexteriorimages/630x420/Hyundai/Verna/7729/1616055133475/front-left-side-47.jpg?tr=w-456"
       },
       
       {
-      productTitle:"Technology-4",
-      description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-      productImg:"https://itconsultingindubai.files.wordpress.com/2016/03/choosing-a-technology-solution.jpg"
+      productTitle:"Honda WR-V",
+      description:"1498CC engine,fuel type is diesel,Mileage:16.5 - 23.7 kmpl,Dimensions(L*W*H):3,999*1,734*1,601mm",
+      productImg:"https://stimg.cardekho.com/images/carexteriorimages/630x420/Honda/WR-V/7665/1601969484764/front-left-side-47.jpg?tr=w-456"
       },
       
       {
-      productTitle:"Technology-5",
-      description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-      productImg:"https://wallpaperaccess.com/full/4755970.jpg"
+      productTitle:"Tata Altroz",
+      description:"1220-1497cc engine,fuel type is diesel,Mileage:25.11kmpl, Dimensions(L*W*H):3,990*1,755*1,523mm",
+      productImg:"https://stimg.cardekho.com/images/carexteriorimages/630x420/Tata/Altroz/7247/1578642800962/front-left-side-47.jpg"
       },
       
       {
-      productTitle:"Technology-6",
-      description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-      productImg:"https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX5577975.jpg"
+      productTitle:"Mahindra Marazzo",
+      description:"1497CC engine,fuel type is diesel,Mileage: 17.3 kmpl, Dimensions(L*W*H):4,585*1,866*1,774mm",
+      productImg:"https://stimg.cardekho.com/images/carexteriorimages/630x420/Mahindra/Marazzo/7948/1597419573783/front-left-side-47.jpg"
+      },
+      
+      {
+      productTitle:"Renault KWID",
+      description:"799-999cc engine,fuel type is petrol,Mileage:22.0kmpl, Dimensions(L*W*H):3,731*1,579*1,490mm",
+      productImg:"https://stimg.cardekho.com/images/carexteriorimages/630x420/Renault/KWID/7426/1617274332690/front-left-side-47.jpg"
+      },
+      {
+        productTitle:"Ford Aspire",
+        description:"1194-1499cc engine,fuel type is petrol,Mileage:14.23kmpl, Dimensions(L*W*H):4,384*1,813*1,669mm",
+        productImg:"https://stimg.cardekho.com/images/carexteriorimages/630x420/Ford/Figo-Aspire/7608/1613548799141/front-left-side-47.jpg"
+      },
+      {
+        productTitle:"Nissan Kicks",
+        description:"1330-1498cc engine,fuel type is petrol,Mileage:22.0kmpl, Dimensions(L*W*H):3,731*1,579*1,490mm",
+        productImg:"https://stimg.cardekho.com/images/carexteriorimages/630x420/Nissan/Kicks/7828/1593410136460/front-left-side-47.jpg"
       }
   ]
   productCount:number=0
